f5aa9e6c4599213b60c8b935547cc2e56112bf7c
Peter-Rambeck <peter@indochinaexplorers.com>
2020-05-06 12:44:25 +0200

    Delfiner

--- src/main/java/model/Medlem.java
+++ src/main/java/model/Medlem.java
@@ -2,19 +2,32 @@ package model;
 
 public class Medlem {
 
-    Boolean aktiv;
-    private int medlemID;
-    private String navn;
+    private boolean aktiv;
+    private boolean mand;
+    private boolean senior;
+    private int medlemID; // (fra DB)
+    private String forNavn;
+    private String efterNavn;
     private int alder;
     private String email;
     private String tlfNr;
+    private int balance;
 
-
-    public Medlem() {
-
+    public Medlem(boolean aktiv, boolean mand, boolean senior, String forNavn, String efterNavn, int alder, String email, String tlfNr) {
+        this.aktiv = aktiv;
+        this.mand = mand;
+        this.senior = senior;
+        this.forNavn = forNavn;
+        this.efterNavn = efterNavn;
+        this.alder = alder;
+        this.email = email;
+        this.tlfNr = tlfNr;
+        this.balance = 0;
     }
 
-
+    public void setMedlemID(int medlemID) {
+        this.medlemID = medlemID;
+    }
 }
 
 
