8236dc8e71f79abab4a03cb81d338a8154abdf10
Peter-Rambeck <peter@indochinaexplorers.com>
2020-05-07 12:23:05 +0200

    Delfiner

--- src/main/java/model/Medlem.java
+++ src/main/java/model/Medlem.java
@@ -38,6 +38,7 @@ public class Medlem {
         this.tlfNr = tlfNr;
         this.balance = 0;
     }
+
     public void gemIDB(){
         MedlemMapper medlemMapper = new MedlemMapper();
         this.setMedlemID(medlemMapper.createNewmedlem(this));
