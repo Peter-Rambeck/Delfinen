337f0b2af3356664a0f8a007909230cc3f1602ba
jensgelbek <jensgelbek@gmail.com>
2020-05-07 09:46:19 +0200

    tr

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,12 @@ import model.Medlem;
 public class Main {
 
     public static void main(String[] args) {
-        // Medlem medlem=new Medlem(true,true,true,"jens","gelbek",48,"fndslg@mflkr.com","3535353");
-        // MedlemMapper mm=new MedlemMapper();
-        // System.out.println( mm.createNewmedlem(medlem));
+         Medlem medlem=new Medlem(true,true,true,"jens","gelbek",48,"fndslg@mflkr.com","3535353");
+        MedlemMapper mm=new MedlemMapper();
+         System.out.println( mm.createNewmedlem(medlem));
 
-        Menu menu = new Menu();
-        menu.hovedMenu();
+      //  Menu menu = new Menu();
+        //menu.hovedMenu();
 
 
     }
