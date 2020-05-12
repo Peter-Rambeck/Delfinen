5ad39b0a42a681fd83834717d29207175fad6af9
jensgelbek <jensgelbek@gmail.com>
2020-05-12 13:41:26 +0200

    tr

--- Delifinen.sql
+++ Delifinen.sql
@@ -6,7 +6,7 @@ CREATE TABLE `Delfin`.`medlem` (
   medlemID INT NOT NULL AUTO_INCREMENT,
   forNavn VARCHAR(45) NULL,
   efterNavn VARCHAR(45) NULL,
-  fodselsaer int NULL,
+  fodselsaer INT NULL,
   email VARCHAR(45) NULL,
   tlfnr VARCHAR(45) NULL,
   balance int NULL,
@@ -21,17 +21,18 @@ CREATE TABLE `Delfin`.`medlem` (
   konkurrenceID INT NOT NULL AUTO_INCREMENT,
   navn VARCHAR(45) NULL,
   dato DATE,
-  PRIMARY KEY (`konkurrenceID`));
-
-
+  PRIMARY KEY (`konkurrenceID`));  
+ 
+  
   CREATE TABLE `Delfin`.`Resultat` (
   resultatID INT NOT NULL AUTO_INCREMENT,
   medlemID INT NULL,
   konkurrenceID INT NULL,
-  disciplin ENUM("bryst","crawl","rygcrawl","butterfly"),
-  tid TIME(3),
+  disciplin INT,
+  tid INT,
   FOREIGN KEY (medlemID) REFERENCES medlem(medlemID),
   FOREIGN KEY (konkurrenceID) REFERENCES konkurrence(konkurrenceID),
   PRIMARY KEY (resultatID));
-
-
+    
+    
+  
