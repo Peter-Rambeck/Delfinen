4a7df7c36b36471561ff4e8c9e1ffa6e0b894d17
jensgelbek <jensgelbek@gmail.com>
2020-05-12 14:08:10 +0200

    tr

--- src/main/java/datamapper/ResultatMapper.java
+++ src/main/java/datamapper/ResultatMapper.java
@@ -4,19 +4,9 @@ import Util.DBConnector;
 import model.*;
 
 import java.sql.*;
-import java.time.LocalTime;
 
-import static model.KonkurrenceResultat.*;
-import static model.KonkurrenceResultat.Svoemmediciplin.*;
 
-/*
-private int konkurrenceResultatID;
-    private KonkurrrenceMedlem Medlem;
-    private Konkurrence konkurrence;
-    public Svoemmediciplin svoemmediciplin;
-    private LocalTime tid;
 
- */
 public class ResultatMapper {
     public int createKonkurrenceResultat(KonkurrenceResultat konkurrenceResultat) {
 
@@ -24,7 +14,7 @@ public class ResultatMapper {
         String sqlQuery = "";
         Connection conn = DBConnector.getInstance().getConnection();
 
-        //laver ny ordre..
+        //laver nyt resultat..
         sqlQuery = "Insert into konkurrenceresultat (medlemID,konkurrenceID,disciplin,tid) " +
                 "Values(\"" +
                 konkurrenceResultat.getMedlem().getMedlemID()+"\",\"" +
@@ -68,39 +58,15 @@ public class ResultatMapper {
                 resultatID=res.getInt("resultatID");
                 int medlemID=res.getInt("medlemID");
                 int konkurrenceID=res.getInt("konkurrenceID");
-                String stDisciplin=res.getString("disciplin");
-                Svoemmediciplin svoemmediciplin;
-                switch (stDisciplin){
-                    case "bryst":
-                        svoemmediciplin= brystsvoemning;break;
-                    case "crawl":
-                        svoemmediciplin=crawl;break;
-                    case "butterfly":
-                        svoemmediciplin=butterfly;break;
-                    case "rygcrawl":
-                        svoemmediciplin=rygcrawl;break;
-                    default:
-                        svoemmediciplin=crawl;
+                int svoemmediciplin=res.getInt("konkurrenceID");
+
 
                 }
-                // to enum
-                Timestamp sqlTid = res.getTimestamp("tid");
-                LocalTime tid=sqlTid.toLocalDateTime().toLocalTime();
-
-                // sæt resultatet sammen
-                KonkurrenceResultat tmpRes=new KonkurrenceResultat( medlemsLister.medlemMap.get(medlemID),
-                                                                    konkurrencer.konkurrenceMap.get(konkurrenceID),
-                                                                   svoemmediciplin,
-                                                                   tid);
-                // tilføj resultat til resultat map..med reultatID som key
-                tmpRes.setKonkurrenceResultatID(resultatID);
-                KonkurrenceResultater.konkurrenceResultatMap.put(resultatID,tmpRes);
 
 
 
 
 
-            }
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
