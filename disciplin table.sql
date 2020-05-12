bfac23ad9f81cc70c6db0453152a09643359b7ea
Peter-Rambeck <peter@indochinaexplorers.com>
2020-05-12 21:40:13 +0200

    Delfiner

--- disciplin table.sql
+++ disciplin table.sql
@@ -2,14 +2,7 @@ create table Delfin.`disciplin` (
   crawl int,
   brystsvoemning int,
   rygcrawl int,
-  butterfly int,
-  primary key (`disciplinID`));
+  butterfly int);
   
   insert into disciplin (crawl, brystsvoemning, rygcrawl, butterfly )
-  value (1);
-  insert into disciplin (brystsvoemning)
-  value (2);
-  insert into disciplin (rygcrawl)
-  value (3);
-  insert into disciplin (butterfly)
-  value (4);
+  value (1, 2, 3, 4);
