21c15ac1e136b9f953ea7ab0858eb88f5ab67a0d
Peter-Rambeck <peter@indochinaexplorers.com>
2020-05-13 23:00:40 +0200

    Delfiner

--- src/main/java/IOMenu/IOOpretMedlem.java
+++ src/main/java/IOMenu/IOOpretMedlem.java
@@ -14,110 +14,192 @@ public class IOOpretMedlem {
                 Scanner opretMedlemInput = new Scanner(System.in);
 
                 // Aktiv eller passiv, Bool
-                System.out.println("medlemstype: ");
-                System.out.println("Aktiv: 1 ");
-                System.out.println("Passiv: 2 ");
-
                 boolean aktiv = false;
-                try {
-                while (opretMedlemInput.hasNextInt()) {
+                while ( aktiv == false ) {
+                        System.out.println("medlemstype: ");
+                        System.out.println("Aktiv: 1 ");
+                        System.out.println("Passiv: 2 ");
 
-                        int InputAktiv = opretMedlemInput.nextInt();
-                        if (InputAktiv > 0 && InputAktiv < 3) {
-                                // boolean aktiv;
-                                if (InputAktiv == 1) {
-                                        aktiv = true;
+                        if (opretMedlemInput.hasNextInt()) {
+                                int inputAktiv = opretMedlemInput.nextInt();
+                                if (inputAktiv > 0 && inputAktiv < 3) {
+                                        // boolean aktiv;
+                                        if (inputAktiv == 1) {
+                                                aktiv = true;
+                                        } else {
+                                                aktiv = false;
+                                                break;
+                                        }
                                 } else {
-                                        aktiv = false;
+                                        System.out.println("Fejltastning, 1 eller 2");
+                                        opretMedlemInput.reset();
                                 }
                         } else {
-                                System.out.println("Fejltastning, 1 eller 2");
+                                System.out.println("Fejltast");
+                                opretMedlemInput.nextLine();
                         }
                 }
-                } catch (InputMismatchException e) {
-                        System.out.println("Her");
-                }
-
 
                 // Mand eller Kvinde, Bool
+                boolean mand = false;
+                while ( mand == false ) {
                         System.out.println("medlemstype: ");
                         System.out.println("Mand: 1 ");
                         System.out.println("Kvinde: 2 ");
-                        int InputMand = opretMedlemInput.nextInt();
-                        boolean mand;
-                        if (InputMand == 1) {
+                        if (opretMedlemInput.hasNextInt()) {
+                                int inputMand = opretMedlemInput.nextInt();
+                                if (inputMand > 0 && inputMand < 3) {
+                        // boolean mand;
+                        if (inputMand == 1) {
                                 mand = true;
                         } else {
                                 mand = false;
+                                break;
                         }
+                } else {
+                        System.out.println("Fejltastning, 1 eller 2");
+                        opretMedlemInput.reset();
+                }
+                        } else {
+                System.out.println("Fejltast");
+                opretMedlemInput.nextLine();
+                        }
+                }
 
-                        // Senior eller Junior, Bool
+                // Senior eller Junior, Bool
+                boolean senior = false;
+                while ( senior == false ) {
                         System.out.println("medlemstype: ");
                         System.out.println("Senior: 1 ");
                         System.out.println("Junior: 2 ");
-                        int InputSenior = opretMedlemInput.nextInt();
-                        boolean senior;
-                        if (InputSenior == 1) {
-                                senior = true;
-                        } else {
-                                senior = false;
-                        }
 
-                        // Motionist eller Konkurrence medlem, Bool
+                        if (opretMedlemInput.hasNextInt()) {
+                                int inputSenior = opretMedlemInput.nextInt();
+                                if (inputSenior > 0 && inputSenior < 3) {
+                                        if (inputSenior == 1) {
+                                                senior = true;
+                                        } else {
+                                                senior = false;
+                                                break;
+                                        }
+                                } else {
+                                        System.out.println("Fejltastning, 1 eller 2");
+                                        opretMedlemInput.reset();
+                                }
+                        } else {
+                                System.out.println("Fejltast");
+                                opretMedlemInput.nextLine();
+                        }
+                }
+
+                // Motionist eller Konkurrence medlem, Bool
+                boolean motionist = false;
+                while ( motionist == false ) {
                         System.out.println("medlemstype: ");
                         System.out.println("Motionist: 1 ");
                         System.out.println("Konkurrencemedlem: 2 ");
-                        int InputMotionist = opretMedlemInput.nextInt();
-                        boolean motionist;
-                        if (InputMotionist == 1) {
-                                motionist = true;
+
+                        if (opretMedlemInput.hasNextInt()) {
+                                int inputMotionist = opretMedlemInput.nextInt();
+                                if (inputMotionist > 0 && inputMotionist < 3) {
+                                        inputMotionist = opretMedlemInput.nextInt();
+                                        if (inputMotionist == 1) {
+                                                motionist = true;
+                                                // opretMedlemInput.nextLine();
+                                        } else {
+                                                motionist = false;
+                                                // opretMedlemInput.nextLine();
+                                                break;
+                                        }
+                                } else {
+                                        System.out.println("Fejltastning, 1 eller 2");
+                                        //opretMedlemInput.nextLine();
+                                }
                         } else {
-                                motionist = false;
+                                System.out.println("Fejltast");
+                                //opretMedlemInput.nextLine();
                         }
+                }
 
-                        // Fornavn, String
-                        System.out.println("Fornavn?: ");
-                        String forNavn = opretMedlemInput.nextLine();
-                        forNavn = opretMedlemInput.nextLine();
-                        System.out.println();
-
-                        // Efternavn, String
-                        System.out.println("Efternavn?: ");
-                        String efterNavn = opretMedlemInput.nextLine();
-                        System.out.println();
-
-                        // Traener, String
-                        // System.out.println("Efternavn?: ");
-
-                        String traener;
-                        if (motionist == true) {
-                                traener = "Bruce Lee";
-                        } else {
-                                traener = "";
-                        }
-
-                        // Alder, int
-                        System.out.println("Fødselsår?: ");
-                        int fodselsaer = opretMedlemInput.nextInt();
-                        System.out.println();
+                // Fornavn, String
+                String forNavn = null;
+                while ( forNavn == null ) {
                         opretMedlemInput.nextLine();
+                        System.out.println("Fornavn?: ");
+                        if( opretMedlemInput.hasNextLine()) {
+                                forNavn = opretMedlemInput.nextLine();
+                        } else {
+                                System.out.println("Fejltastning");
+                                //opretMedlemInput.reset();
+                                }
+                }
 
-                        // Email, String
+                // Efternavn, String
+                String efterNavn = null;
+                while ( efterNavn == null ) {
+                        System.out.println("Efternavn?: ");
+                        if( opretMedlemInput.hasNextLine()) {
+                                efterNavn = opretMedlemInput.nextLine();
+                        } else {
+                                System.out.println("Fejltastning");
+                                //opretMedlemInput.reset();
+                        }
+                }
+
+                // Traener, String -> Hardcodet
+                String traener;
+                if (motionist == true) {
+                        traener = "Bruce Lee";
+                } else {
+                        traener = "";
+                }
+
+                // Alder, int
+                int fodselsaer = 0;
+                while ( fodselsaer == 0 ) {
+                        System.out.println("Fødselsår?: ");
+                        if (opretMedlemInput.hasNextInt()) {
+                                fodselsaer = opretMedlemInput.nextInt();
+                                //System.out.println();
+                                opretMedlemInput.nextLine();
+                        } else {
+                                System.out.println("Fejltastning");
+                                opretMedlemInput.nextLine();
+                        }
+                }
+
+                // Email, String
+                String email = null;
+                while ( email == null ) {
                         System.out.println("email?: ");
-                        String email = opretMedlemInput.nextLine();
-                        System.out.println();
+                        if (opretMedlemInput.hasNextLine()) {
+                                email = opretMedlemInput.nextLine();
 
-                        // Telefon, String
+                        } else {
+                                System.out.println("Fejltastning");
+                               // opretMedlemInput.nextLine();
+                        }
+                }
+
+                // Telefon, String
+                String tlfNr = null;
+                while ( tlfNr == null ) {
                         System.out.println("Telefon nr.?: ");
-                        String tlfNr = opretMedlemInput.nextLine();
+                        if (opretMedlemInput.hasNextLine()) {
+                                tlfNr = opretMedlemInput.nextLine();
+                                // opretMedlemInput.nextLine();
+                        } else {
+                                System.out.println("Fejltastning");
+                                //opretMedlemInput.nextLine();
+                        }
+                }
 
-                        // Balance, int
-                        int balance = 0;
+                // Balance, int
+                int balance = 0;
 
-
-                        Medlem medlem = new Medlem(aktiv, mand, senior, motionist, forNavn, efterNavn, traener, fodselsaer, email, tlfNr, balance);
-                        medlem.gem();
-                        System.out.println(medlem);
+                Medlem medlem = new Medlem(aktiv, mand, senior, motionist, forNavn, efterNavn, traener, fodselsaer, email, tlfNr, balance);
+                medlem.gem();
+                System.out.println(medlem);
 
                 }
 
