3a19023c7055c339ea420cad43f4d1046a1f59e2
Peter-Rambeck <peter@indochinaexplorers.com>
2020-05-13 11:14:40 +0200

    Delfiner

--- medlem.sql
+++ medlem.sql
@@ -1,25 +1,24 @@
-use Delfin;
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ("Poul","Larsen",1973,"poul@larsen.dk", "11223344", 473, 1, 1, 1);
+    use Delfin;
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ("Poul","Larsen",1973,"poul@larsen.dk", "11223344", 473, 1, 1, 1);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Beate","Nielsen",1980,"beate@nielsen.dk","23456677", 253, 1, 0, 1);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Beate","Nielsen",1980,"beate@nielsen.dk","23456677", 253, 1, 0, 1);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Ingolf","Frederiksen", 1952,"ingolf@frederiksen.dk","24534722", 654, 1, 1, 1);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Ingolf","Frederiksen", 1952,"ingolf@frederiksen.dk","24534722", 654, 1, 1, 1);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Bettina", "Glistrup", 1960, "bettina@glistrup.dk", "55432367", 543, 1, 0, 1);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Bettina", "Glistrup", 1960, "bettina@glistrup.dk", "55432367", 543, 1, 0, 1);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Lars", "Jensen", 2004,"lars@jensen.dk", "45677543", 4, 1, 1, 0);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Lars", "Jensen", 2004,"lars@jensen.dk", "45677543", 4, 1, 1, 0);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Michael","Hansen",2003,"michael@hansen.dk", "24768894", 132, 1, 1, 0);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Michael","Hansen",2003,"michael@hansen.dk", "24768894", 132, 1, 1, 0);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Marc", "Petersen", 2005,"marc@petersen.dk","45336721", -166, 0,1, 0);
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Marc", "Petersen", 2005,"marc@petersen.dk","45336721", -166, 0,1, 0);
 
-INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
-VALUES ( "Niels", "Nielsen", 2008, "niels@nielsen.dk", "54987623", 145, 1, 1, 0);
- 
+    INSERT INTO medlem (forNavn, efterNavn, fodselsaer, email, tlfnr, balance, statu, koen, aldersklasse)
+    VALUES ( "Niels", "Nielsen", 2008, "niels@nielsen.dk", "54987623", 145, 1, 1, 0);
